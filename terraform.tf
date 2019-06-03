2b4726e4814c8a3bd401304f1b6b6870012e4d0c
Alexander Mancevice <amancevice@cargometrics.com>
2019-06-03 19:56:01 -0400

    correct statefile name

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   backend s3 {
     bucket = "brutalismbot"
-    key    = "terraform/brutalismbot.com.tf"
+    key    = "terraform/brutalismbot.com.tfstate"
     region = "us-east-1"
   }
 
